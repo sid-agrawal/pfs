27b8b0a1696329ceba472df5d83e60481c3120dc
Sid Agrawal <sid@sid-agrawal.ca>
2024-10-23 18:46:57 -0700

    minor changese to mmpa example

--- osmosis_examples/hello_mmap.c
+++ osmosis_examples/hello_mmap.c
@@ -6,7 +6,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define MMAP_SIZE 4096 * 3
+#define PAGE_SIZE 4096
+#define MMAP_SIZE PAGE_SIZE * 3
 #define MMAP_PATH "shm_path"
 
 int main() {
@@ -16,15 +17,31 @@ int main() {
 
    // map it
    __attribute__((unused)) int fd = ftruncate(shm_fd, MMAP_SIZE); 
-   void *mem = mmap(NULL, MMAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+   size_t mmap_sz = PAGE_SIZE;
+   off_t file_off = PAGE_SIZE;
+
+   void *mem = mmap(NULL, mmap_sz, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, file_off);
    assert(mem != MAP_FAILED);
 
-   printf("Hello! mmap'ed [%p,%p]\n", mem, (char*)mem + MMAP_SIZE);
+   printf("Hello! mmap'ed [%p,%p] : SX : %lx\n", mem, (char*)mem + mmap_sz, mmap_sz);
+
+   mmap_sz = MMAP_SIZE;
+   file_off = 0;
+
+   void *mem2 = mmap(NULL, mmap_sz, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, file_off);
+   assert(mem != MAP_FAILED);
+
+   printf("Hello! mmap'ed [%p,%p] : SX : %lx\n", mem2, (char*)mem2 + mmap_sz, mmap_sz);
 
    int counter = 0;
    while (1) {
       counter = counter % 0x10000000;
-      for (volatile int *i = mem; (char *) i < (char *) mem + MMAP_SIZE; i++) {
+      for (volatile int *i = mem; (char *) i < (char *) mem + PAGE_SIZE; i++) {
+         *i |= counter;
+         counter++;
+      }
+      for (volatile int *i = mem2; (char *) i < (char *) mem2 + MMAP_SIZE; i++) {
          *i |= counter;
          counter++;
       }
