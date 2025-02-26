f7d2961a157b23b40972137c891d1911ccda9eba
Sid Agrawal <sid@sid-agrawal.ca>
2025-02-25 20:58:56 -0800

    add kv_app_in_vm

---
+++ osmosis_examples/kv_app_in_vm.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdatomic.h>
+#include <unistd.h>
+#include <time.h>
+#include "kv_inmemory.h"
+#include <sys/mman.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <termios.h>
+#include <sys/types.h>
+
+
+// App
+void *generator(void *arg) {
+    shared_buffer_t *shared_buffer = (shared_buffer_t *)arg;
+
+    while (1) {
+        shared_buffer->cmd = rand() % 2 == 0 ? GET : SET;
+        if (shared_buffer->cmd == SET) {
+            shared_buffer->key = rand() %10;
+            shared_buffer->value = rand() %100;
+        } else {
+            shared_buffer->key = rand() %10;
+        }
+        atomic_store(&shared_buffer->message_ready, 1);
+
+        while (!atomic_load(&shared_buffer->result_ready)) {
+            // Busy-wait
+        }
+        if (shared_buffer->cmd == SET) {
+            printf("SET: Success\n");
+        } else {
+            if (shared_buffer->result != -1 ) {
+                printf("GET: Success. Value : %d\n", shared_buffer->result);
+            } else {
+                printf("GET: Error : %d\n", shared_buffer->result);
+            }
+        }
+        atomic_store(&shared_buffer->result_ready, 0);
+
+        // Sleep for a random time between 1 and 2 seconds
+        sleep(1 + rand() % 2);
+    }
+
+    return NULL;
+}
+
+// Consumer
+#define KVS_VM_SHARED_PAGE_HOST_PA 0x5f600000
+
+int main() {
+    pthread_t thread1, thread2;
+    shared_buffer_t shared_buffer;
+
+    atomic_init(&shared_buffer.message_ready, 0);
+    atomic_init(&shared_buffer.result_ready, 0);
+
+    
+    /* Map Dev Mem at  0x5f600000 */
+    if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
+        FATAL;
+    }
+        
+    printf("/dev/mem opened.\n"); 
+    fflush(stdout);
+
+    /* Map one page */
+    size_t map_size = getpagesize();
+    size_t map_mask = map_size - 1;
+    map_base = mmap(0, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, target & ~map_mask);
+    if(map_base == (void *) -1) {
+            FATAL; 
+    } else {
+            printf("Memory mapped at address %p.\n", map_base); 
+    }
+    fflush(stdout);
+
+    virt_addr = (char * )map_base + (target & MAP_MASK);
+
+    generator(virt_addr);
+
+    return 0;
+}
